f6486258a39365d60c8b3a6862b20a9ffba82f41
liwdaw <liwocha.dawid@gmail.com>
2017-12-22 11:45:37 +0100

    added config.xml

--- src/main/java/com/liwdaw/Main.java
+++ src/main/java/com/liwdaw/Main.java
@@ -1,9 +1,18 @@
 package com.liwdaw;
 
+import java.nio.file.FileSystems;
+
+import com.liwdaw.util.XMLFile;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		try {
+			XMLFile xmlFile = new XMLFile(FileSystems.getDefault().getPath("config.xml"));
+			xmlFile.setNodeList("source");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}		
 	}
 
 }
